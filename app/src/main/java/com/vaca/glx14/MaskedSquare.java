d54e65a9934dbd69ab221659a4a571b1bfd9f8b7
liguanghao <liguanghao@viatomtech.com>
2022-01-25 17:45:58 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/glx14/MaskedSquare.java
@@ -0,0 +1,29 @@
+package com.vaca.glx14;
+
+
+import com.vaca.glx14.glkit.ShaderProgram;
+
+/**
+ * Created by burt on 2016. 6. 25..
+ */
+public class MaskedSquare extends Model {
+
+    static final float vertices[] = {
+             1.0f, -1.0f, 0f,   1f, 0f,
+             1.0f,  1.0f, 0f,   1f, 1f,
+            -1.0f,  1.0f, 0f,   0f, 1f,
+            -1.0f, -1.0f, 0f,   0f, 0f
+
+    };
+
+    static final short indices[] = {
+            0, 1, 2,
+            2, 3, 0
+    };
+
+
+
+    public MaskedSquare(ShaderProgram shader) {
+        super("MaskedSquare", shader, vertices, indices);
+    }
+}
